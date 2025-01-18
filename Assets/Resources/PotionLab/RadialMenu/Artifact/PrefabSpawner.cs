74b87e7034e4c29cbb950b5aea3f89af00e0f4b6
Thi Thanh Tu DUONG <77581509+luvluvdt3@users.noreply.github.com>
2025-01-18 12:05:25 +0100

    On going object spawn

---
+++ Assets/Resources/PotionLab/RadialMenu/Artifact/PrefabSpawner.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+public class PrefabSpawner : MonoBehaviour
+{
+    [System.Serializable]
+    public class PrefabData
+    {
+        public GameObject prefabToSpawn;
+        public Sprite image;
+        public UnityEvent onSelected;
+    }
+
+    public GameObject prefabUI;
+    public PrefabData[] prefabDataList;
+
+    void Start()
+    {
+        SpawnPrefabUI();
+    }
+
+    void SpawnPrefabUI()
+    {
+        if (prefabUI == null)
+        {
+            Debug.LogError("PrefabUI template is missing!");
+            return;
+        }
+
+        foreach (PrefabData data in prefabDataList)
+        {
+            if (data.prefabToSpawn == null)
+            {
+                Debug.LogError("PrefabToSpawn is missing in PrefabData!");
+                continue;
+            }
+
+            GameObject uiInstance = Instantiate(prefabUI, transform);
+
+            Button button = uiInstance.GetComponent<Button>();
+            if (button != null)
+            {
+                button.onClick.AddListener(() => OnPrefabButtonClicked(data,button.transform.position));
+            }
+            else
+            {
+                Debug.LogError("PrefabUI does not contain a Button component!");
+            }
+
+            Transform imageTransform = uiInstance.transform.Find("Image");
+            if (imageTransform != null)
+            {
+                Image imageComponent = imageTransform.GetComponent<Image>();
+                if (imageComponent != null)
+                {
+                    imageComponent.sprite = data.image;
+                }
+                else
+                {
+                    Debug.LogError("Image child does not have an Image component!");
+                }
+            }
+            else
+            {
+                Debug.LogError("PrefabUI does not contain a child named 'Image'!");
+            }
+        }
+    }
+
+    public void OnPrefabButtonClicked(PrefabData data, Vector3 position)
+    {
+        Instantiate(data.prefabToSpawn, position, Quaternion.identity);
+        data.onSelected?.Invoke();
+    }
+}
